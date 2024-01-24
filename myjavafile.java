1344e848526fe193e9c74d984ffc2899699b7a6b
srisatya14 <srisatya14.mca@gmail.com>
2024-01-24 07:34:35 +0530

    my task completed

--- myjavafile.java
+++ myjavafile.java
@@ -1,2 +1,10 @@
 this is my java file
+<<<<<<< Updated upstream
 this is manager work completed
+=======
+1st day of java lines of code completed
+2nd day of java lines of code completed
+3rd day of java lines of code completed
+4th day of java lines of code completed
+5th day of java lines of code completed
+
